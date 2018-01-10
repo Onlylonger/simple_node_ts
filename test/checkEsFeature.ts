6a5405f3b46aa391935749fff097ea5e0bc06491
王世隆 <547463736@qq.com>
2018-01-10 11:49:36 +0800

    U

--- test/checkEsFeature.ts
+++
@@ -1,8 +0,0 @@
-import * as ReflectSupports from 'es-feature-tests'
-
-ReflectSupports('all', (results, timestamp) => {
-  /* tslint:disable no-console */
-  console.log(`当前node 版本: ${process.version}`)
-  console.log(results)
-  /* tslint:enable no-console */
-})
